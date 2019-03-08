bc1c3ac82758034e2dac377bf1aa2ca2eb91fa78
marinus <marinus@laurabo.dk>
2019-03-08 11:38:05 +0100

    Played with drawing, programing 1 module 8-3 2019

--- Drawing.js
+++ Drawing.js
@@ -6,8 +6,8 @@ let myRec, browserCompatible, pen, direction, displayWord;
 
 
 function setup() {
-    cnv = createCanvas(400, 400);
-    background('red');
+    cnv = createCanvas(800, 600);
+    background('black');
     //Check browser compatibility
     browserCompatible = window.webkitSpeechRecognition ||
         window.mozSpeechRecognition ||
@@ -23,50 +23,64 @@ function setup() {
         myRec.start();
     }    
     displayWord = createDiv();
+    pen = {
+        x: width / 2,
+        y: height / 2,
+        size: 8,
+        col: color(248, 248, 255, 150),
+        show: function(){
+            fill(this.col),
+            ellipseMode(CENTER),
+            ellipse(this.x, this.y, this.size)
+        }
+    }
+    console.log("pen findes, og dens x værdi er: " + pen.x);
 }
 
 function draw() {
+    pen.show();
 }
 
 function showResult() {
     if (myRec.resultValue == true) {
         word = myRec.resultString.split(' ').pop();
         displayWord.html(word.toLowerCase());
+        switch(word){
+            case 'left':
+                direction = "left"
+                break;
+            case 'right':
+                direction = "right"
+                break;
+            case 'up':
+                direction = "up"
+                break;
+            case 'down':
+                direction = "down"
+                break;
+            default:
+                break;
+        }
     }
 }
 
 /*
 OPGAVER 
-
 Vi skal forsøge at lave et lille tegneprogram, som bruger stemmegenkendelse til at føre pennen rundt på skærmen. 
-
 Det første vi gør er at lave et objekt, pen, med Javascript Object notation. Objektet skal have følgende properties og metoder:
-
 x, y, size, col
 show() - metode som laver fill(this.col), og viser en ellipse(this.x, this.y, this.size, this.size)
-
 Se hvordan her: https://www.w3schools.com/jS/js_objects.asp
-
 Objektet skal laves i setup() - og metoden pen.show() skal kaldes i draw() 
-
 Nu skal vi så bruge variablen word til at flytte rundt på objektet. Til det skal vi bruge et såkaldt switch statement i draw. 
-
 Læs dokumentationen her - og se om du kan sørge for at ordene 'left', 'right', 'up', 'down' flytter vores pen rundt
 https://www.w3schools.com/js/js_switch.asp
-
 Se så om du kan tilføje endnu en metode i objektet - bounce - som sørger for at pennen ikke kommer længere, 
 når x er mindre end nul eller større end bredden. Og tilsvarende for y.
-
 Bemærk at der findes en smart måde at skrive comditions på, således:
-
 a = a < 0 ? 0 : a;
-
 Som betyder - sæt a til (er a mindre end nul?) 0, ellers a
-
 _ _ _ _ _ _ _ _
-
 Se om du kan lave programmet så man kan skifte farver - ved for eksempel at sige green, red, blue
-
 Se om du kan lave programmet så man kan skifte størrelse ved at sige bigger, smaller
-
 */
