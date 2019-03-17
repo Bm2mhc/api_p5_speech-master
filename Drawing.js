bf1080b1ebf7e940f591ed098ec98c152f963cd4
Bm2mhc <47781200+Bm2mhc@users.noreply.github.com>
2019-03-17 17:49:58 +0100

    Update Drawing.js

--- Drawing.js
+++ Drawing.js
@@ -2,7 +2,9 @@
 //reference: http://ability.nyu.edu/p5.js-speech/
 //Cloned from github.com/simmoe/api_p5_speech/
 
-let myRec, browserCompatible, pen, direction, displayWord, timer, whattimeisit = 1000;
+let myRec, browserCompatible, pen, direction, displayWord, timer;
+let dick = false;
+let LOL = false;
 
 
 function setup() {
@@ -44,17 +46,45 @@ function setup() {
     console.log("pen findes, og dens x værdi er: " + pen.x);
 }
 
-function dick1() {
-
-    console.log("hej");
-}
 
 function draw() {
     if (direction == "left") pen.x -= 1;
     if (direction == "up") pen.y -= 1;
     if (direction == "down") pen.y += 1;
     if (direction == "right") pen.x += 1;
+    if (direction == "dick") {
+        if (!dick) {
+            dick = true;
+            timer = millis();
+        }
+        let time = millis() - timer;
 
+        if (time < 6000) {
+            pen.x ++;
+        }
+        if (time > 6000 && time < 6500) {
+            pen.y++;
+        }
+        if (time > 6500 && time < 12500) {
+            pen.x--;
+        }
+        if (time > 12500 && time < 13500) {
+            pen.y++;
+        }
+        if (time > 13500 && time < 14500) {
+            pen.x--;
+        }
+        if (time > 14500 && time < 16000){
+            pen.y--;
+        }
+        if (time > 16000 && time < 17000){
+            pen.x++;
+        }
+        if (time > 17000) {
+            houseDrawing = false;
+        }
+    }
+    if (direction == "LOL") createDiv("LOL");
 
     pen.bounce();
     pen.show();
@@ -87,32 +117,16 @@ function showResult() {
                 pen.size -= 1;
                 break;
             case 'did':
+            case 'dik':
             case 'dick':
                 direction = "dick"
+                break;
+            case 'laugh':
+            case 'lol':
+                direction = "LOL"
                 break;
             default:
                 direction = "stop"
         }
     }
 }
-
-/*
-OPGAVER 
-Vi skal forsøge at lave et lille tegneprogram, som bruger stemmegenkendelse til at føre pennen rundt på skærmen. 
-Det første vi gør er at lave et objekt, pen, med Javascript Object notation. Objektet skal have følgende properties og metoder:
-x, y, size, col
-show() - metode som laver fill(this.col), og viser en ellipse(this.x, this.y, this.size, this.size)
-Se hvordan her: https://www.w3schools.com/jS/js_objects.asp
-Objektet skal laves i setup() - og metoden pen.show() skal kaldes i draw() 
-Nu skal vi så bruge variablen word til at flytte rundt på objektet. Til det skal vi bruge et såkaldt switch statement i draw. 
-Læs dokumentationen her - og se om du kan sørge for at ordene 'left', 'right', 'up', 'down' flytter vores pen rundt
-https://www.w3schools.com/js/js_switch.asp
-Se så om du kan tilføje endnu en metode i objektet - bounce - som sørger for at pennen ikke kommer længere, 
-når x er mindre end nul eller større end bredden. Og tilsvarende for y.
-Bemærk at der findes en smart måde at skrive comditions på, således:
-a = a < 0 ? 0 : a;
-Som betyder - sæt a til (er a mindre end nul?) 0, ellers a
-_ _ _ _ _ _ _ _
-Se om du kan lave programmet så man kan skifte farver - ved for eksempel at sige green, red, blue
-Se om du kan lave programmet så man kan skifte størrelse ved at sige bigger, smaller
-*/
